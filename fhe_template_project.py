e6bf99b3b008fa81ebefd3ac1831cf9e7a7d1014
tugcetumer <34840300+tugcetumer@users.noreply.github.com>
2022-04-15 15:14:17 +0300

    simcnt is updated as 100

--- fhe_template_project.py
+++ fhe_template_project.py
@@ -135,7 +135,7 @@ def simulate(n):
 
 
 if __name__ == "__main__":
-    simcnt = 3 #The number of simulation runs, set it to 3 during development otherwise you will wait for a long time
+    simcnt = 100 #The number of simulation runs, set it to 3 during development otherwise you will wait for a long time
     # For benchmarking you must set it to a large number, e.g., 100
     #Note that file is opened in append mode, previous results will be kept in the file
     resultfile = open("results.csv", "a")  # Measurement results are collated in this file for you to plot later on

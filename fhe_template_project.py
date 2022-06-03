1c96e6b95febed5b11b75e814379268936b20ed1
tugcetumer <34840300+tugcetumer@users.noreply.github.com>
2022-06-03 10:30:29 +0300

    algorithm is implemented

--- fhe_template_project.py
+++ fhe_template_project.py
@@ -6,11 +6,16 @@ import timeit
 import networkx as nx
 from random import random
 
+numberofvertices = 0
+visited = []
+edges = []
+
 # Using networkx, generate a random graph
 # You can change the way you generate the graph
 def generateGraph(n, k, p):
     #ws = nx.cycle_graph(n)
     ws = nx.watts_strogatz_graph(n,k,p)
+
     return ws
 
 # If there is an edge between two vertices its weight is 1 otherwise it is zero
@@ -19,18 +24,34 @@ def generateGraph(n, k, p):
 # Assume there are n vertices
 # (i,j)th element of the adjacency matrix corresponds to (i*n + j)th element in the vector representations
 def serializeGraphZeroOne(GG,vec_size):
+
+    global edges
+
     n = GG.size()
+    #n = 4
     graphdict = {}
     g = []
+
+    w, h = n, n
+    edges = [[-1 for x in range(w)] for y in range(h)]
+
     for row in range(n):
+        counter = 0
         for column in range(n):
-            if GG.has_edge(row, column) or row==column: # I assumed the vertices are connected to themselves
+            if GG.has_edge(row, column):
+                weight = 1
+                edges[row][counter] = column
+                counter += 1
+            elif row == column:
                 weight = 1
             else:
                 weight = 0 
             g.append( weight  )  
             key = str(row)+'-'+str(column)
             graphdict[key] = [weight] # EVA requires str:listoffloat
+
+    # print(edges)
+
     # EVA vector size has to be large, if the vector representation of the graph is smaller, fill the eva vector with zeros
     for i in range(vec_size - n*n): 
         g.append(0.0)
@@ -52,15 +73,63 @@ def prepareInput(n, m):
     input['Graph'] = graph
     return input
 
+def mask(graph, node, adj):
+
+    dummyList = []
+    for i in range(16384):
+        if i == node*4 + adj:
+            dummyList.append(1)
+        else:
+            dummyList.append(0)
+
+    masked = graph * dummyList
+
+    return masked    
+
+# Circle checking method
+# Every visited vertex is appended to visited vector
+# New vertex is checked
+def checkCircle(adj):
+
+    for i in range(len(visited)):
+        if adj == visited[i]:
+            return True
+    return False
+
+
 # This is the dummy analytic service
 # You will implement this service based on your selected algorithm
 # you can other parameters using global variables !!! do not change the signature of this function 
 def graphanalticprogram(graph):
-    reval = graph<<1 ## Check what kind of operators are there in EVA, this is left shift
-    # Note that you cannot compute everything using EVA/CKKS
-    # For instance, comparison is not possible
-    # You can add, subtract, multiply, negate, shift right/left
-    # You will have to implement an interface with the trusted entity for comparison (send back the encrypted values, push the trusted entity to compare and get the comparison output)
+
+    # An index variable, used for edge
+    e = 0
+
+    # Result vector initialize
+    reval = []
+    for i in range(16384):
+        reval.append(0)
+
+    # For circle check
+    # Vertex 0 is visited first
+    visited.append(0)
+
+    # Number of edges to be taken is equal to V-1
+    while e < numberofvertices - 1:
+
+        adj = 0
+        while (edges[e][adj] == -1 or checkCircle(edges[e][adj])) and adj < numberofvertices - 1:
+            adj += 1
+
+        if (adj != numberofvertices - 1):
+            visited.append(edges[e][adj])
+
+            masked = mask(graph, e, edges[e][adj])
+
+            reval = reval + masked
+
+        e += 1
+
     return reval
     
 # Do not change this 
@@ -81,7 +150,14 @@ class EvaProgramDriver(EvaProgram):
 # n is the number of nodes in your graph
 # If you require additional parameters, add them
 def simulate(n):
+
+    global numberofvertices
+    global visited
+
     m = 4096*4
+
+    numberofvertices = n
+
     print("Will start simulation for ", n)
     config = {}
     config['warn_vec_size'] = 'false'
@@ -95,10 +171,10 @@ def simulate(n):
         graph = Input('Graph')
         reval = graphanalticprogram(graph)
         Output('ReturnedValue', reval)
-    
+
     prog = graphanaltic
-    prog.set_output_ranges(30)
-    prog.set_input_scales(30)
+    prog.set_output_ranges(60)
+    prog.set_input_scales(60)
 
     start = timeit.default_timer()
     compiler = CKKSCompiler(config=config)
@@ -126,8 +202,9 @@ def simulate(n):
     referenceexecutiontime = (timeit.default_timer() - start) * 1000.0 #ms
     
     # Change this if you want to output something or comment out the two lines below
-    for key in outputs:
-        print(key, float(outputs[key][0]), float(reference[key][0]))
+    for i in range (16):
+        for key in outputs:
+            print(key, float(outputs[key][i]), float(reference[key][i]))
 
     mse = valuation_mse(outputs, reference) # since CKKS does approximate computations, this is an important measure that depicts the amount of error
 
@@ -139,17 +216,19 @@ if __name__ == "__main__":
     # For benchmarking you must set it to a large number, e.g., 100
     #Note that file is opened in append mode, previous results will be kept in the file
     resultfile = open("results.csv", "a")  # Measurement results are collated in this file for you to plot later on
-    resultfile.write("NodeCount,PathLength,SimCnt,CompileTime,KeyGenerationTime,EncryptionTime,ExecutionTime,DecryptionTime,ReferenceExecutionTime,Mse\n")
+    resultfile.write("NodeCount,SimCnt,CompileTime,KeyGenerationTime,EncryptionTime,ExecutionTime,DecryptionTime,ReferenceExecutionTime,Mse\n")
     resultfile.close()
     
     print("Simulation campaing started:")
-    for nc in range(36,64,4): # Node counts for experimenting various graph sizes
+    for nc in range(12,60,4): # Node counts for experimenting various graph sizes
         n = nc
         resultfile = open("results.csv", "a") 
         for i in range(simcnt):
+            visited.clear()
+            #print(edges)
             #Call the simulator
             compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse = simulate(n)
             res = str(n) + "," + str(i) + "," + str(compiletime) + "," + str(keygenerationtime) + "," +  str(encryptiontime) + "," +  str(executiontime) + "," +  str(decryptiontime) + "," +  str(referenceexecutiontime) + "," +  str(mse) + "\n"
-            print(res)
+            #print(res)
             resultfile.write(res)
         resultfile.close()
